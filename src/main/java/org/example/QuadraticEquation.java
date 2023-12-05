925163fc1a9d78f5b0fbd57f4262e7874cb7bea6
Timoshenko Veronika <timohadarws@gmail.com>
2023-12-05 21:44:54 +0600

    Change Array to ArrayList Write test

--- src/main/java/org/example/QuadraticEquation.java
+++ src/main/java/org/example/QuadraticEquation.java
@@ -1,41 +1,54 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuadraticEquation { //ax^2+bx+c=0
-    private int a;
-    private int b;
-    private int c;
+    private double a;
+    private double b;
+    private double c;
 
     public QuadraticEquation(int a, int b, int c) {
-        if(a==0){
-            throw  new IllegalArgumentException("Не квадратный трехчлен");
-        }
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
-    public int getA() {
+    public double getA() {
         return a;
     }
 
-    public int getB() {
+    public double getB() {
         return b;
     }
 
-    public int getC() {
+    public double getC() {
         return c;
     }
 
-    public double [] solveEquation(){
-        double[] rootsArray = new double[2];
-        double D = b*b-4*a*c;
-        if(D<0){
-            rootsArray = null;
+    public List<Double> solveEquation() {
+        List<Double> roots = new ArrayList<>();
+        if (a == 0) {
+            roots.add(-c / b);
+            return roots;
         }
-        if(D>=0){
-            rootsArray[0] = (-b+Math.sqrt(D))/(2*a);
-            rootsArray[1] = (-b-Math.sqrt(D))/(2*a);
+        double D = b * b - 4 * a * c;
+        if (D == 0) {
+            roots.add(-b / (2 * a));
         }
-        return rootsArray;
+        if (D > 0) {
+            roots.add((-b + Math.sqrt(D)) / (2 * a));
+            roots.add((-b - Math.sqrt(D)) / (2 * a));
+        }
+        return roots;
+    }
+
+    @Override
+    public String toString() {
+        return "QuadraticEquation{" +
+                "a=" + a +
+                ", b=" + b +
+                ", c=" + c +
+                '}';
     }
 }
